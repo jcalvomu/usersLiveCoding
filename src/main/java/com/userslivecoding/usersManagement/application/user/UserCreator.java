6dfd931b6e5fa31ac95a1cb11535236e9109d73d
Calvo Muñoz, Juan <jcalvomu@minsait.com>
2020-04-01 17:42:50 +0200

    Añadida excecpción al crear dos usuarios iguales

--- src/main/java/com/userslivecoding/usersManagement/application/user/UserCreator.java
+++ src/main/java/com/userslivecoding/usersManagement/application/user/UserCreator.java
@@ -3,6 +3,8 @@ package com.userslivecoding.usersManagement.application.user;
 import com.userslivecoding.usersManagement.domain.user.User;
 import com.userslivecoding.usersManagement.domain.user.UserDTO;
 import com.userslivecoding.usersManagement.domain.user.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -11,13 +13,19 @@ import java.util.Optional;
 public class UserCreator {
 
     private final UserRepository userRepository;
+    private final Logger logger = LoggerFactory.getLogger(UserCreator.class);
 
     public UserCreator(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
     public UserDTO exec(UserDTO userToCreate) {
-        if (doUserExist(userToCreate)) return null;
+        if (doUserExist(userToCreate)) {
+            String error = String.format("User %s with age %s allready exists",
+                    userToCreate.getName(), userToCreate.getAge());
+            logger.error(error);
+            throw new RuntimeException(error);
+        }
         User user = new User(-1, userToCreate.getName(), userToCreate.getAge());
         User createdUser = userRepository.save(user);
         return new UserDTO(createdUser.getName(), createdUser.getAge());
