710127a7d0139dabcc23ae9a71de21d6c5f4a221
Calvo Muñoz, Juan <jcalvomu@minsait.com>
2020-04-01 09:08:31 +0200

    First commit

---
+++ src/test/java/com/userslivecoding/usersManagement/UsersManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.userslivecoding.usersManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
